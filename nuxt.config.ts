5e5f9ac9daaae3cff997f99507b929fe312c4b6f
PraZ <sachabouillez@gmail.com>
2022-02-20 21:21:34 +0100

    :construction_worker: enable lambda nitro preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ import Icons from 'unplugin-icons/vite'
 import Components from 'unplugin-vue-components/vite'
 
 export default defineNuxtConfig({
+  nitro: {
+    preset: 'lambda',
+  },
   privateRuntimeConfig: {
     RECAPTCHA_SECRET: process.env.RECAPTCHA_SECRET,
   },
