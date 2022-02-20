25f1aadd8ad0cd00601d8ed67894f91572f47039
PraZ <sachabouillez@gmail.com>
2022-02-20 01:36:00 +0100

    :sparkles: add partners and members

--- components/SectionTitle.vue
+++ components/SectionTitle.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
-defineProps<{ h1?: boolean }>()
+defineProps<{ h1?: boolean, label: string }>()
 </script>
 
 <template>
-  <div w:mb="4" w:flex="~" w:justify="center">
+  <div w:text="2xl" w:mb="4" w:flex="~" w:justify="center">
     <component
       :is="h1 ? 'h1' : 'h2'"
+      w:m="0"
       w:text="2xl"
       w:font="normal"
       w:border="b-2 secondary"
     >
-      <slot></slot>
+      {{ label }}
     </component>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
-const { descriptionText, membersText, membersMembers } = useContent()
+const { descriptionText, membersText, membersMembers, partners } = useContent()
 </script>
 
 <template>
-  <section>
+  <section class="space-y-12" w:text="center">
     <!-- Description -->
     <div w:flex="~ col sm:row" w:space-y="2">
       <div w:mr="sm:4" w:w="sm:2/3">
-        <SectionTitle h1>
-          {{ "Qu'est ce que l'AREPAC?" }}
-        </SectionTitle>
+        <SectionTitle h1 label="Qu'est ce que l'AREPAC?" />
         <div w:text="left">
           {{ descriptionText }}
         </div>
@@ -20,20 +18,42 @@ const { descriptionText, membersText, membersMembers } = useContent()
       </div>
     </div>
     <!-- Members -->
-    <div w:flex="~ col" w:align="center">
-      <SectionTitle>
-        {{ "Les membres du bureau" }}
-      </SectionTitle>
+    <div w:flex="~ col" w:align="items-center">
+      <SectionTitle label="Les membres du bureau" />
       <div w:mb="2">
         {{ membersText }}
       </div>
-      <img w:w="sm:3/4" src="" alt="Photo des membres du bureau" />
-      <div w:text="italic">
+      <img
+        src="~/assets/images/membres.webp"
+        alt="Photo des membres du bureau"
+        w:w="sm:3/4"
+      />
+      <div w:font="italic" w:w="sm:3/4">
         {{ membersMembers }}
       </div>
     </div>
-
     <!-- Partners -->
+    <div>
+      <SectionTitle label="Les partenaires" />
+      <div w:grid="~ cols-2 gap-4 sm:cols-3 md:cols-4">
+        <a 
+          v-for="partner in partners"
+          target="_blank"
+          rel="noreferrer noopener"
+          :href="partner.link"
+          :key="partner.name"
+          w:flex="~"
+          w:align="items-center"
+          w:h="full"
+          w:p="2"
+        >
+          <img 
+            :alt="partner.name"
+            src="~/assets/images/fake.webp"
+            w:w="full" />
+        </a>
+      </div>
+    </div>
     <!-- Newsletter -->
   </section>
 </template>
